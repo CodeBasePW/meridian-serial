72a87b5ce92dc8761ab6f05aeca0d071f71685d8
CodeBasePW <coderatorman@gmail.com>
2019-11-14 18:30:32 -0800

    Added readStream func for reading internal files from JAR

--- src/me/moderator_man/meridian/serial/FormatReader.java
+++ src/me/moderator_man/meridian/serial/FormatReader.java
@@ -4,14 +4,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
+@SuppressWarnings("unchecked")
 public class FormatReader<T>
 {
-	@SuppressWarnings("unchecked")
 	public T read(String path)
 	{
 		try
 		{
 			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File(path))))
+			{
+				return (T) ois.readObject();
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public T readStream(String uri)
+	{
+		try
+		{
+			try (ObjectInputStream ois = new ObjectInputStream(FormatReader.class.getResourceAsStream(uri)))
 			{
 				return (T) ois.readObject();
 			}
